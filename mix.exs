534bd7f2f336781ef876fe26504a101705cc4eed
Medhi Widjaja <medhiwidjaja@yahoo.com>
2021-11-08 11:34:38 +0700

    Update version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BambooMailjet.Mixfile do
   def project do
     [
       app: :bamboo_mailjet,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
